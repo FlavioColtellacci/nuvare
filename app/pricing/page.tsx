2293754712fafdb3dc7c43fe0755fdd3cb1b0846
Flavio Coltellacci <coltellaccif@gmail.com>
2026-03-06 11:06:06 +0100

    Add back button to pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,12 +1,18 @@
 import PricingClient from "@/app/pricing/pricing-client";
+import BackButton from "@/components/BackButton";
 
 export default function PricingPage() {
   return (
-    <PricingClient
-      coreMonthlyPriceId={process.env.STRIPE_CORE_MONTHLY_PRICE_ID ?? ""}
-      coreYearlyPriceId={process.env.STRIPE_CORE_YEARLY_PRICE_ID ?? ""}
-      professionalMonthlyPriceId={process.env.STRIPE_PROFESSIONAL_MONTHLY_PRICE_ID ?? ""}
-      professionalYearlyPriceId={process.env.STRIPE_PROFESSIONAL_YEARLY_PRICE_ID ?? ""}
-    />
+    <div className="relative">
+      <div className="absolute top-6 left-6 z-50">
+        <BackButton />
+      </div>
+      <PricingClient
+        coreMonthlyPriceId={process.env.STRIPE_CORE_MONTHLY_PRICE_ID ?? ""}
+        coreYearlyPriceId={process.env.STRIPE_CORE_YEARLY_PRICE_ID ?? ""}
+        professionalMonthlyPriceId={process.env.STRIPE_PROFESSIONAL_MONTHLY_PRICE_ID ?? ""}
+        professionalYearlyPriceId={process.env.STRIPE_PROFESSIONAL_YEARLY_PRICE_ID ?? ""}
+      />
+    </div>
   );
 }
